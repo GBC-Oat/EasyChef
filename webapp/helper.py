7ecc21d2085be6f4ce8d866b11333f2af351fd7a
Pat <124858798+pat-nb@users.noreply.github.com>
2024-04-16 21:08:19 -0400

    Update support heif and heic image file

--- webapp/helper.py
+++ webapp/helper.py
@@ -1,16 +1,17 @@
+from io import BytesIO
+from PIL import Image
+from pillow_heif import read_heif
 import logging
 from flask import render_template, request, jsonify, session
 import requests
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-INGREDIENTS_DETECTION_API_URL = 'http://138.197.140.119:8000/detect/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'heif', 'heic'}
+INGREDIENTS_DETECTION_API_URL = 'http://138.197.140.119:8000/detect_ingredients/'
 RECIPE_API_URL = 'http://138.197.140.119:8000/find_recipe/'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-TEMP = []
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -21,13 +22,42 @@ def detect_objects():
     
     uploaded_file = request.files['image']
 
+    if uploaded_file.filename == '':
+        return "No selected file", 400
+
     if uploaded_file and allowed_file(uploaded_file.filename):
-        files = {'image': (uploaded_file.filename, uploaded_file.read())}
-        logger.info(f"Sending request to {INGREDIENTS_DETECTION_API_URL} with files: {files}")
+
+        image_stream = BytesIO(uploaded_file.read())
+
+        if uploaded_file.filename.lower().endswith('.heic', '.heif'):
+            # Decode the HEIC image
+            heic_image = read_heif(image_stream)
+            
+            # Convert the HEIC image to a PIL Image
+            img = Image.frombytes(
+                heic_image.mode,
+                heic_image.size,
+                heic_image.data,
+                "raw",
+                heic_image.mode,
+                heic_image.stride,
+            )
+        else:
+            # Otherwise, assume it's already in JPEG or PNG format
+            img = Image.open(image_stream)
+
+        # Convert image to bytes
+        img_bytes = BytesIO()
+        img.save(img_bytes, format='JPEG')
+
+        # Create files dictionary
+        files = {'image': (uploaded_file.filename, img_bytes.getvalue())}
+
+        logger.info(f"Sending request to {INGREDIENTS_DETECTION_API_URL} with image: {uploaded_file.filename}")
         response = requests.post(INGREDIENTS_DETECTION_API_URL, files=files)
 
         if response.status_code == 200:
-            detected_ingredients = response.json().get('detected_labels', [])
+            detected_ingredients = response.json().get('detected_ingredients', [])
             session['detected_ingredients'] = detected_ingredients
             logger.info(f"Received response: {response.json()}")
             return render_template('list_ingredients.html', detected_ingredients=detected_ingredients)
@@ -41,8 +71,10 @@ def detect_objects():
 
 def find_recipe():
     detected_ingredients = session.get('detected_ingredients', [])
+    if not detected_ingredients:
+        return render_template('index.html', message="Please detect ingredients first.")
+    
     logger.info(f"Sending request to {RECIPE_API_URL} with ingredients: {detected_ingredients}")
-
     response = requests.post(RECIPE_API_URL, json={'ingredients': detected_ingredients})
 
     if response.status_code == 200:
